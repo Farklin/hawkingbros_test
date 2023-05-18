f8860dcbedec02e6270ec7261d5fbaa3988d8522
farklin <ivanhel@yandex.ru>
2023-05-18 17:30:25 +0300

    промежуточное сохранение

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index(Request $request)
     {
-        
+
+        $artciles = Article::all(); 
+        return view('article', compact('artciles')); 
     }
 }
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -14,4 +14,9 @@ class Article extends Model
     {
         return $this->belongsToMany(Tag::class, 'article_tags', 'article_id', 'tag_id' ); 
     }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
